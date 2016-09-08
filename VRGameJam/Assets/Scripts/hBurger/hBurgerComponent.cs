9d681674ec42649ff6c50460903707984ca1703b
Xiaoshan Hill <hilllocanton@gmail.com>
2016-09-08 16:44:50 -0400

    Add BurgerFactory

---
+++ VRGameJam/Assets/Scripts/hBurger/hBurgerComponent.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class hBurgerComponent : MonoBehaviour {
+
+    [SerializeField]
+    private Rigidbody _Rigidbody;
+
+    [SerializeField]
+    private h_PickableObject _h_PickableObject;
+    
+    private GameObject _InteractionPoint;
+
+    [SerializeField]
+    public List<GameObject> _AboveBurgerComponents;
+
+    public bool HasBeenAttached = false;
+
+    [SerializeField]
+    public hBurgerComponent CurrentBase;
+
+    void Start()
+    {
+        this._AboveBurgerComponents.Add(this.gameObject);
+        this.CurrentBase = this;
+    }
+
+    void Update()
+    {
+        if(!this._InteractionPoint && this.transform.FindChild("InteractionPoint"))
+            this._InteractionPoint = this.transform.FindChild("InteractionPoint").gameObject;
+
+    }
+
+    public void AttachToBurger(bool hasTop = false)
+    {
+        this.HasBeenAttached = true;
+        Destroy(this._Rigidbody);
+        Destroy(this._h_PickableObject);
+        Destroy(this._InteractionPoint);
+        //Destroy(this);
+    }
+}
---
+++ VRGameJam/Assets/Scripts/hBurger/hBurgerDetectingZone.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hBurgerDetectingZone : MonoBehaviour {
+
+    [SerializeField]
+    private Transform _Identity;
+
+    [SerializeField]
+    private Collider _Collider;
+
+    [SerializeField]
+    private hBurgerComponent _BurgerComponent;
+
+    [SerializeField]
+    private bool AutoAdjustment = true;	
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "BurgerComponent")
+        {
+            hBurgerComponent burgerComponent = other.GetComponent<hBurgerComponent>();
+            if(burgerComponent && !burgerComponent.HasBeenAttached)
+            {
+                if (AutoAdjustment)
+                {
+                    Vector3 newPos = this.gameObject.transform.position;
+                    newPos.y += ( other.transform.localScale.y / 2.0f );
+                    other.gameObject.transform.position = newPos;
+
+                    Quaternion newQuat = Quaternion.Euler(this.gameObject.transform.rotation.eulerAngles.x, other.gameObject.transform.rotation.eulerAngles.y, this.gameObject.transform.rotation.eulerAngles.z);
+
+                    other.gameObject.transform.rotation = newQuat;
+                }
+                other.gameObject.transform.SetParent(this._Identity);
+                burgerComponent.AttachToBurger();
+
+                // Register all burger components above in this (current) gameObject's hBurgerComponent._AboveBurgerComponents
+                hBurgerComponent currentBaseBurgerComponent = this._BurgerComponent.CurrentBase.GetComponent<hBurgerComponent>();
+                currentBaseBurgerComponent._AboveBurgerComponents.AddRange(burgerComponent.CurrentBase.GetComponent<hBurgerComponent>()._AboveBurgerComponents);
+
+                // Change every bases of components above to this (current) gameObject
+                foreach(GameObject childrenBurgerComponent in burgerComponent._AboveBurgerComponents)
+                {
+                    childrenBurgerComponent.GetComponent<hBurgerComponent>().CurrentBase = this._BurgerComponent.CurrentBase;
+                }
+
+                // Remove Detecting Zone
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
